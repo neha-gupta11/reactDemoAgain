0e73aeb6f92c0d89c2f2787412bc0ae199e114e0
Neha Gupta <neha.gupta@tothenew.com>
2017-11-17 14:57:05 +0530

    Updated the webpack.config.js'

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,17 @@
 const path=require('path');
 
 module.exports={
-    entry:'./first.js',
+    entry:'./src/first.js',
     output:{
         // to move the bundle file inside dist folder.
         path:path.resolve(__dirname,'dist'),
         filename:'bundle.js'
     },
     module:{
-        loaders: [{
+        rules: [{
             test: /\.js/,
-            use: ['babel-loader'],
             exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
+            loader: 'babel-loader'
         }
         ]
     }
